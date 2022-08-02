1ee419460f73377e1bb1d92adb32e63084bf2928
Emmanuel <emmaokpiaifo@gmail.com>
2022-08-02 16:23:01 +0100

    js

---
+++ js/scripts.js
@@ -0,0 +1,124 @@
+$(document).ready(function(){
+
+  $(".clickable").click(function(){
+      $(".card-title").toggle();
+      $(".card-text").toggle()
+  });
+
+  $(".clickable2").click(function(){
+    $(".card2-title").toggle();
+    $(".card2-text").toggle();
+});
+}); 
+
+$(document).ready(function(){
+
+  $(".clickable3").click(function(){
+      $(".card3-title").toggle();
+      $(".card3-text").toggle()
+  });
+
+  $(".clickable4").click(function(){
+    $(".card4-title").toggle();
+    $(".card4-text").toggle();
+});
+});
+
+$(document).ready(function(){
+
+  $(".clickable5").click(function(){
+      $(".card5-title").toggle();
+      $(".card5-text").toggle()
+  });
+
+  $(".clickable6").click(function(){
+    $(".card6-title").toggle();
+    $(".card6-text").toggle();
+});
+}); 
+
+$(document).ready(function(){
+
+  $(".clickable7").click(function(){
+      $(".card7-title").toggle();
+      $(".card7-text").toggle()
+  });
+
+  $(".clickable8").click(function(){
+    $(".card8-title").toggle();
+    $(".card8-text").toggle();
+});
+}); 
+
+$(document).ready(function(){
+
+  $(".clickable9").click(function(){
+      $(".card9-title").toggle();
+      $(".card9-text").toggle()
+  });
+
+  $(".clickable10").click(function(){
+    $(".card10-title").toggle();
+    $(".card10-text").toggle();
+});
+}); 
+
+$(document).ready(function(){
+
+  $(".clickable11").click(function(){
+      $(".card11-title").toggle();
+      $(".card11-text").toggle()
+  });
+
+  $(".clickable12").click(function(){
+    $(".card12-title").toggle();
+    $(".card12-text").toggle();
+});
+}); 
+
+$(document).ready(function(){
+
+  $(".clickable13").click(function(){
+      $(".card13-title").toggle();
+      $(".card13-text").toggle()
+  });
+
+  $(".clickable14").click(function(){
+    $(".card14-title").toggle();
+    $(".card14-text").toggle();
+});
+}); 
+
+$(document).ready(function(){
+
+  $(".clickable15").click(function(){
+      $(".card15-title").toggle();
+      $(".card15-text").toggle()
+  });
+
+  $(".clickable16").click(function(){
+    $(".card16-title").toggle();
+    $(".card16-text").toggle();
+});
+}); 
+
+$(document).ready(function(){
+
+  $(".clickable17").click(function(){
+      $(".card17-title").toggle();
+      $(".card17-text").toggle()
+  });
+
+  $(".clickable18").click(function(){
+    $(".card18-title").toggle();
+    $(".card18-text").toggle();
+});
+}); 
+
+$(document).ready(function(){
+
+  $(".clickable19").click(function(){
+      $(".card19-title").toggle();
+      $(".card19-text").toggle()
+  });
+}); 
